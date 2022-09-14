56202d99617391ec8fcce041d0730c409aa3e62c
RDBagwell <erbagwell@yahoo.com>
2022-09-14 13:59:08 -0600

    Implament Auth

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
-import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core'
+import {useState} from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import useStyles from './styles'
+import Input from './Input';
 
 const Auth = () => {
-    const state = null;
+    const isSignup = false;
+    const classes = useStyles();
+    const [showPassword, setShowPassword] = useState(false)
+    
+    const handleSubmit = ()=>{
+
+    }
+
+    const handleChange = ()=>{
+
+    }
+
+    const handleShowPassword = ()=> setShowPassword((prevShowPassword)=> !prevShowPassword)
   return (
-    <div>Auth</div>
+    <Container>
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar } >
+          <LockOutlinedIcon/>
+        </Avatar>
+        <Typography variant='h5' >
+          {isSignup ? 'Sign Up' : 'Sign In'}
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {
+              isSignup && (
+                <>
+                  <Input name='firstName' label="First Name" handleChange={handleChange}  autoFocus  half />
+                  <Input name='lastName' label="Last Name" handleChange={handleChange} half />
+                </> 
+              )
+            }
+            <Input name='email' label="Email Address" handleChange={handleChange} type='email' />
+            <Input name='password' label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+            {isSignup && <Input name='confirmPassword' label="Repeat Password" handleChange={handleChange} type="password" /> }
+            <Button type='submit' variant='contained' color='primary' className={classes.submit} fullWidth>
+              {isSignup ? 'Sign Up' : 'Sign In'}
+            </Button>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
   )
 }
 
